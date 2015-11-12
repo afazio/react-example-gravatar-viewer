ab6dba76c23121dd873f701dda5a7deead641f7f
Alfred J. Fazio <alfred.fazio@gmail.com>
2015-11-12 16:06:35 -0500

    updated EmailInput component to keep state.  now updates in response to prop changes.

--- src/client/components/EmailInput.js
+++ src/client/components/EmailInput.js
@@ -6,11 +6,28 @@ class EmailInput extends React.Component {
     event.preventDefault();
     return false;
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.email
+    };
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  componentWillReceiveProps(next) {
+    this.setState({value: next.email});
+  }
   
   render() {
+    const value = this.state.value;
+
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
-        <input type="email" ref="email" defaultValue={this.props.email} />
+        <input type="email" ref="email" value={value} onChange={this.handleChange.bind(this)} />
       </form>
     );
   }
